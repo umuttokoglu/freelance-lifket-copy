da7dcdef213e1429576af20e3fc0b95739da3745
Umut Tokoğlu <umuttokoglu@gmail.com>
2025-03-24 17:42:56 +0300

    version added for css

--- resources/views/shared/guest/css.blade.php
+++ resources/views/shared/guest/css.blade.php
@@ -3,10 +3,10 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@300;400;600&display=swap">
 <!-- css -->
-<link rel="stylesheet" href="{{ asset('assets/guest/css/fontawesome.min.css') }}"/>
-<link rel="stylesheet" href="{{ asset('assets/guest/css/linearicons.min.css') }}"/>
-<link rel="stylesheet" href="{{ asset('assets/guest/css/aos.css') }}"/>
-<link rel="stylesheet" href="{{ asset('assets/guest/css/style.css') }}"/>
-<link rel="stylesheet" href="{{ asset('assets/guest/css/custom.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/guest/css/fontawesome.min.css') }}?v=1"/>
+<link rel="stylesheet" href="{{ asset('assets/guest/css/linearicons.min.css') }}?v=1"/>
+<link rel="stylesheet" href="{{ asset('assets/guest/css/aos.css') }}?v=1"/>
+<link rel="stylesheet" href="{{ asset('assets/guest/css/style.css') }}?v=1"/>
+<link rel="stylesheet" href="{{ asset('assets/guest/css/custom.css') }}?v=1"/>
 
 @yield('pageCss')
