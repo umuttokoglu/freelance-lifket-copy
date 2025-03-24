c703c08134b82c43faad3effc87df0f9712361b8
Umut Tokoğlu <umuttokoglu@gmail.com>
2025-03-24 17:40:08 +0300

    fix

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -73,19 +73,6 @@
                                     </td>
                                     <td class="text-center">
                                         <div class="action-btns">
-                                            <a href="{{ route('guest.hizmetler.index', ['hizmetler' => $category]) }}" class="action-btn btn-view bs-tooltip me-2"
-                                               data-toggle="tooltip" data-placement="top"
-                                               title="{{ __('admin/category.index.table.th.actions.view') }}"
-                                               target="_blank">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                     stroke-width="2"
-                                                     stroke-linecap="round" stroke-linejoin="round"
-                                                     class="feather feather-eye">
-                                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                    <circle cx="12" cy="12" r="3"></circle>
-                                                </svg>
-                                            </a>
                                             <a href="{{ route('admin.category.edit', $category) }}"
                                                class="action-btn btn-edit bs-tooltip me-2"
                                                data-toggle="tooltip" data-placement="top"
